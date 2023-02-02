a977e71740df2672b24c437679478d487483a156
Filip Horvat <filip.horvat@fer.hr>
2023-02-02 19:04:20 +0100

    solved

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let sum: u32 = (0..).map_while(|i| {
+        let n = fib(i);
+        if n >= 4_000_000 {
+            None
+        } else {
+            Some(n)
+        }
+    }).filter(|n| n % 2 == 0).sum();
+    println!("{sum}");
+}
+
+fn fib(n: u32) -> u32 {
+    match n {
+        0 => 1,
+        1 => 2,
+        n => fib(n - 1) + fib(n - 2),
+    }
+}
